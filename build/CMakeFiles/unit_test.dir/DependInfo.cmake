8b52e9346532ece32428e490941b14be9b720c82
Bhoomika Shirol <76142515+bhoomikashirol@users.noreply.github.com>
2024-08-13 12:38:38 +0530

    Add files via upload

---
+++ build/CMakeFiles/unit_test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bhoomika-s/Downloads/Crc_UT/src/Code.cpp" "CMakeFiles/unit_test.dir/src/Code.cpp.o" "gcc" "CMakeFiles/unit_test.dir/src/Code.cpp.o.d"
+  "/home/bhoomika-s/Downloads/Crc_UT/test/UT/UT.cpp" "CMakeFiles/unit_test.dir/test/UT/UT.cpp.o" "gcc" "CMakeFiles/unit_test.dir/test/UT/UT.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bhoomika-s/Downloads/Crc_UT/build/googletest-main/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/bhoomika-s/Downloads/Crc_UT/build/googletest-main/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake"
+  "/home/bhoomika-s/Downloads/Crc_UT/build/googletest-main/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake"
+  "/home/bhoomika-s/Downloads/Crc_UT/build/googletest-main/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/unit_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_test.dir/src/Code.cpp.o"
+  "CMakeFiles/unit_test.dir/src/Code.cpp.o.d"
+  "CMakeFiles/unit_test.dir/test/UT/UT.cpp.o"
+  "CMakeFiles/unit_test.dir/test/UT/UT.cpp.o.d"
+  "unit_test"
+  "unit_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
