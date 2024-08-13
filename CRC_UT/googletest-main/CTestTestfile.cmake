51e8401ed721bd7f69d871be46c321ef76bfa4b3
Bhoomika Shirol <76142515+bhoomikashirol@users.noreply.github.com>
2024-08-13 12:07:50 +0530

    Add files via upload

---
+++ CRC_UT/googletest-main/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/harshambhonsle/Documents/Gtest_Training/Crc_UT/googletest-main
+# Build directory: /home/harshambhonsle/Documents/Gtest_Training/Crc_UT/googletest-main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
