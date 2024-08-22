a19aee219afd06c2b53ba505d1e44c06987e7a1a
Bhoomika Shirol <76142515+bhoomikashirol@users.noreply.github.com>
2024-08-22 10:09:13 +0530

    Delete Test/CRC_UT/googletest-main directory

--- Test/CRC_UT/googletest-main/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /var/lib/jenkins/workspace/PipelineDemo/Test/CRC_UT/googletest-main/googletest
-# Build directory: /var/lib/jenkins/workspace/PipelineDemo/build/Test/CRC_UT/googletest-main/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
