ec56aeb13c20d14ac46c8c2148e6c3f2f2a78793
Bhoomika Shirol <76142515+bhoomikashirol@users.noreply.github.com>
2024-09-11 11:14:27 +0530

    Update IT.cpp

--- CRC_UT/test/IT/IT.cpp
+++ CRC_UT/test/IT/IT.cpp
@@ -1,35 +1,4 @@
-/****************************************************************************
-* Company:
-* File Name:
-* Author:
-* Version:
-* Date:
-* Operating Environment: 
-* Compiler with Version Number: 
-* Description: Unit Test file for verifying Crc_8 module
-* Requirement ID:
-* LLD ID :
-* Revisers Name:
-* Date:
-* Customer Bug No./ CMF No.:
-* Brief description of the fix/enhancement:
-* Created by Tata Elxsi Ltd., ADAS
-*Copyright 2023 Tata Elxsi Ltd.
-*All rights reserved. 
-This code contains information that is proprietary to Tata Elxsi Ltd. No part of this code may be reproduced or used in whole or part in any form or by any means - graphic, electronic or mechanical without the written permission of Tata Elxsi Ltd. 
-
-****************************************************************************/
-
-/*****************************************************************************
-* Function Name: Crc_8_TC_01
-* Description: calculate an 8-bit cyclic redundancy check (CRC) value with     first call to calculate the CRC is TRUE
-* Parameters: 
-* Called functions: 
-* Global Data: Also describe any global data manipulated through pointers.
-* Return Value: 
-* Exceptions: 
-*****************************************************************************/
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include <sstream>
 #include "Code.h"
 
@@ -51,4 +20,3 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
